da1422627fa7c648293db2ef450b7807d9104a1f
crmgg <claryrivillaas@gmail.com>
2026-04-29 20:42:43 -0500

    Cambio de To a TO

--- src/main/java/co/edu/uco/ucoparking/ucoparking/features/student/registernewstudent/application/inputport/RegisterNewStudentInputPort.java
+++ src/main/java/co/edu/uco/ucoparking/ucoparking/features/student/registernewstudent/application/inputport/RegisterNewStudentInputPort.java
@@ -1,7 +1,7 @@
 package co.edu.uco.ucoparking.ucoparking.features.student.registernewstudent.application.inputport;
 
 import co.edu.uco.ucoparking.ucoparking.application.usecase.inputport.InputPort;
-import co.edu.uco.ucoparking.ucoparking.features.student.registernewstudent.application.inputport.to.input.RegisterNewStudentInputTo;
+import co.edu.uco.ucoparking.ucoparking.features.student.registernewstudent.application.inputport.to.input.RegisterNewStudentInputTO;
 
-public interface RegisterNewStudentInputPort extends InputPort<RegisterNewStudentInputTo, Void> {
+public interface RegisterNewStudentInputPort extends InputPort<RegisterNewStudentInputTO, Void> {
 }
