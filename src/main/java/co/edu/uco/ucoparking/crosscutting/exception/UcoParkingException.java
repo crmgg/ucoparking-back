708bbf1a984f678ce846bd380bf09948aea9ac8b
crmgg <claryrivillaas@gmail.com>
2026-05-14 20:31:50 -0500

    Commit creacion de crosscutting

---
+++ src/main/java/co/edu/uco/ucoparking/crosscutting/exception/UcoParkingException.java
@@ -0,0 +1,17 @@
+package co.edu.uco.ucoparking.crosscutting.exception;
+
+public class UcoParkingException extends RuntimeException {
+    private static final long serialVersionUID = 1L;
+
+    public UcoParkingException(String message) {
+        super(message);
+    }
+
+    public UcoParkingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UcoParkingException(Throwable cause) {
+        super(cause);
+    }
+}
