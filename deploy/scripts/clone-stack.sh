f0687ee12d4392610c7fb827ad8f687f502d6304
crmgg <claryrivillaas@gmail.com>
2026-05-31 20:15:17 -0500

    fix: clone-stack sin sudo y config-server en master

--- deploy/scripts/clone-stack.sh
+++ deploy/scripts/clone-stack.sh
@@ -6,25 +6,26 @@ set -euo pipefail
 GITHUB_USER="${GITHUB_USER:?Define GITHUB_USER (ej: crmgg)}"
 TARGET="${TARGET:-/opt/uco-parking}"
 BRANCH="${BRANCH:-feature/notification-gateway}"
+CONFIG_SERVER_BRANCH="${CONFIG_SERVER_BRANCH:-master}"
 
-sudo mkdir -p "$TARGET"
-sudo chown "$USER:$USER" "$TARGET"
+mkdir -p "$TARGET"
 cd "$TARGET"
 
 clone_or_pull() {
   local dir="$1"
   local repo="$2"
+  local branch="$3"
   if [[ -d "$dir/.git" ]]; then
     git -C "$dir" fetch origin
-    git -C "$dir" reset --hard "origin/$BRANCH"
+    git -C "$dir" reset --hard "origin/$branch"
   else
-    git clone --branch "$BRANCH" --depth 1 "https://github.com/${GITHUB_USER}/${repo}.git" "$dir"
+    git clone --branch "$branch" --depth 1 "https://github.com/${GITHUB_USER}/${repo}.git" "$dir"
   fi
 }
 
-clone_or_pull uco-parking ucoparking-back
-clone_or_pull UcoParkingFront ucoParking-front
-clone_or_pull uco-parking-config-server uco-parking-config-server
+clone_or_pull uco-parking ucoparking-back "$BRANCH"
+clone_or_pull UcoParkingFront ucoParking-front "$BRANCH"
+clone_or_pull uco-parking-config-server uco-parking-config-server "$CONFIG_SERVER_BRANCH"
 
 if [[ ! -d uco-parking/deploy ]]; then
   echo "Falta uco-parking/deploy en el repo ucoparking-back."
